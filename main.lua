97ba2283e5beb78ad9d053cf8d79aa88a9c02f42
David Corcuera <david.corcuera@gmail.com>
2023-04-01 19:36:17 +0200

    use a State Machine to handle game: Title State and Play State

--- main.lua
+++ main.lua
@@ -28,6 +28,12 @@ require 'Pipe'
 -- pipePair class we've written
 require 'PipePair'
 
+-- all code related to game state and state machines
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/PlayState'
+require 'states/TitleScreenState'
+
 -- physical screen dimensions
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 576
@@ -65,6 +71,13 @@ function love.load()
 
     love.window.setTitle("Fifty Bird")
 
+    -- initialize our nice-looking retro text fonts
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
+
     math.randomseed(os.time())
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -72,6 +85,13 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end,
+    }
+    gStateMachine:change('title')
 
     love.keyboard.keysPressed = {}
 end
@@ -97,57 +117,14 @@ function love.keyboard.wasPressed(key)
 end
 
 function love.update(dt)
-    if scrolling then
-        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 
-        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
-        spawnTimer = spawnTimer + dt
+    -- now, we just update the state machine, which defers to the right state
+    gStateMachine:update(dt)
 
-        -- spawn a new PipePair if the timer is past 2 seconds
-        if spawnTimer > 2 then
-            -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
-            -- no higher than 10 pixels below the top edge of the screen,
-            -- and no lower than a gap length (90 pixels) from the bottom
-            local y = math.max(-PIPE_HEIGHT + 10,
-                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-            lastY = y
-
-            table.insert(pipePairs, PipePair(y))
-            spawnTimer = 0
-        end
-
-        -- update the bird for input and gravity
-        bird:update(dt)
-
-        -- for every pipe pair in the scene...
-        for k, pair in pairs(pipePairs) do
-            pair:update(dt)
-
-            -- check to see if bird collided with pipe
-            for l, pipe in pairs(pair.pipes) do
-                if bird:collides(pipe) then
-                    -- pause the game to show collision
-                    scrolling = false
-                end
-            end
-            
-        end
-
-        -- remove any flagged pipes
-        -- we need this second loop, rather than deleting in the previous loop, because
-        -- modifying the table in-place without explicit keys will result in skipping the
-        -- next pipe, since all implicit keys (numerical indices) are automatically shifted
-        -- down after a table removal
-        for k, pair in pairs(pipePairs) do
-            if pair.remove then
-                table.remove(pipePairs, k)
-            end
-        end
-
-
-        love.keyboard.keysPressed = {}
-    end
+    love.keyboard.keysPressed = {}
 end
 
 function love.draw()
@@ -155,15 +132,9 @@ function love.draw()
 
     love.graphics.draw(background, -backgroundScroll, 0)
 
-    for k, pair in pairs(pipePairs) do
-        pair:render()
-    end
+    gStateMachine:render()
 
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
-
-    -- render our bird to the screen using its own render logic
-    bird:render()
-
 
     push:finish()
 end
